3e1d0e8e8cf552454dfa024e141ee48aab31d7a4
vincent van de merwe <335479@student.mboutrecht.nl>
2024-05-20 21:43:10 +0200

    sixth commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\GegevensController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/producten/index', [GegevensController::class, 'index'])->name('producten.index');
+
+Route::post('/producten/index', [GegevensController::class, 'index'])->name('producten.index');
+
+Route::get('/specificatie/index/{product}/{info}/{secondInfo}', [GegevensController::class, 'specificatieIndex'])->name('specificatie.index');
